80a915b5d7b60e830aa2607c194e97327f887214
Soubhab Pathak <dev.soubhab.pathak@gmail.com>
2022-02-10 14:02:21 +0530

    Default Security removed.By Default war file will be generated.

--- 3.Build/1.Springboot/CovidScreening/src/main/java/ac/iit/kgp/covid/screening/CovidScreeningApplication.java
+++ 3.Build/1.Springboot/CovidScreening/src/main/java/ac/iit/kgp/covid/screening/CovidScreeningApplication.java
@@ -2,12 +2,13 @@ package ac.iit.kgp.covid.screening;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 import ac.iit.kgp.covid.screening.properties.FileStorageProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableConfigurationProperties({
     FileStorageProperties.class
 })
